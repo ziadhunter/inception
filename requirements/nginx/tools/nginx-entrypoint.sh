39b3ad191cacc9d28dd879ad00aef16a632a056b
ziadhunter <farouk.ziad.2002@gmail.com>
2026-09-13 17:02:34 +0100

    truying to  setup nginx

---
+++ requirements/nginx/tools/nginx-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p /etc/nginx/ssl
+
+if [ ! -f /etc/nginx/ssl/inception.crt ] || [ ! -f /etc/nginx/ssl/inception.key ]; then
+    echo "Generating SSL certificate..."
+
+    openssl req -x509 -nodes -days 365 \
+        -newkey rsa:2048 \
+        -keyout /etc/nginx/ssl/inception.key \
+        -out /etc/nginx/ssl/inception.crt \
+        -subj "/C=MA/ST=Fes/L=Fes/O=1337/OU=Student/CN=${DOMAIN_NAME}"
+
+    chmod 600 /etc/nginx/ssl/inception.key
+fi
+
+sed -i "s/\${DOMAIN_NAME}/$DOMAIN_NAME/g" /etc/nginx/conf.d/default.conf
+
+echo "Starting NGINX..."
+
+exec nginx -g "daemon off;"
